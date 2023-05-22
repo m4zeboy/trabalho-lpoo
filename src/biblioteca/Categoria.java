4ab2ab3ce78dbb8d222742284eea5e476396a71e
m4zeboy <moises775azevedo@gmail.com>
2023-05-22 17:48:25 -0300

    Criação da classe de categoria

---
+++ src/biblioteca/Categoria.java
@@ -0,0 +1,31 @@
+package biblioteca;
+
+import java.util.Random;
+
+public class Categoria {
+    private int id;
+    private String nome;
+
+    public Categoria(String nome) {
+        this.id = new Random().nextInt(10000);
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String toString() {
+        String saida = "Categoria #" + id + "\n";
+        saida += "Nome: " + nome + "\n";
+        return saida;
+    }
+}
