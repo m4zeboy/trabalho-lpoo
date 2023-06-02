a8ddde91104cd0d723daa62c4219f6aa8d8054cc
m4zeboy <moises775azevedo@gmail.com>
2023-06-02 14:54:43 -0300

    Começando a trocar as operações por operações especializadas no arquivo principal

--- src/biblioteca/Biblioteca.java
+++ src/biblioteca/Biblioteca.java
@@ -2,6 +2,10 @@ package biblioteca;
 
 import biblioteca.emprestimo.Emprestimo;
 import biblioteca.exemplar.Exemplar;
+import biblioteca.operacoes.exemplares.OperacoesDeExemplar;
+import biblioteca.operacoes.exemplares.OperacoesGraficasDeExemplar;
+import biblioteca.operacoes.usuario.OperacoesDeUsuario;
+import biblioteca.operacoes.usuario.OperacoesGraficasDeUsuario;
 import biblioteca.usuario.Usuario;
 
 import javax.swing.JOptionPane;
@@ -15,53 +19,69 @@ public class Biblioteca {
     ArrayList<Emprestimo> emprestimos = new ArrayList<>();
     ArrayList<Reserva> reservas = new ArrayList<>();
 
-    Grafica operacoes = new Grafica();
+    Operacoes menuPrincipal = new Grafica();
 
-    Semente.semear(usuarios, acervo, categorias, emprestimos);
+    Semente.semear(usuarios, acervo, categorias, emprestimos, reservas);
+    // todo A classe Grafica deveria ser separada em outras mais específicas, cada qual gerenciando a sua parte.
+    //    todo classe de operações de exemplares
+    //    todo classe de operações de categorias
+    //    todo classe de operações de emprestimos
+    //    todo classe de operações de reservas
+
+
+    // todo Na classe Biblioteca eu recomendaria o uso do switch para os casos do menu, aumenta a legibilidade do código, especialmente ao utilizar o formato mais atual do JAVA.
+
+    // todo Tecnicamente a data do empréstimo deve ser a data em que o objeto foi criado (data atual). Nesse sentido, o construtor não deveria receber essa data como parametro.
+
+    // todo A classe multa faltou o campo valor conforme especifica o diagrama presente na descrição do trabalho.
+
+    // todo o método estaAtiva da classe reserva poderia ser simplificado, retornando diretamente o resutlado do I
+
+    // todo Recomendo delegar as operações de condicionais para uma classe específica que trate o contexto de cada funcionalidade (Usuario, Categoria, Exemplar, etc)
+
+    // todo A parte de devolução de empréstimos está permitindo a devolução de um item que já foi devolvido anteriormente.
+
+    // todo Sugiro estudarem um pouco sobre o conceito de ENUM e como utilizar em JAVA.
 
     boolean continuar = true;
     while(continuar) {
-      int opcaoPrincipal = operacoes.selecionarOpcaoPrincipal();
+      int opcaoPrincipal = menuPrincipal.selecionarOpcao();
       switch (opcaoPrincipal) {
         case 1:
+          OperacoesDeUsuario operacoesDeUsuario = new OperacoesGraficasDeUsuario();
           boolean continuarUsuarios = true;
           while(continuarUsuarios) {
-            int opcaoDeUsuarios = operacoes.selecionarOpcaoDeUsuarios();
+            int opcaoDeUsuarios = operacoesDeUsuario.selecionarOpcao();
             switch (opcaoDeUsuarios) {
               case 1:
                 /* cadastrar usuário */
-                Usuario novo = operacoes.criarUsuario(usuarios);
+                Usuario novo = operacoesDeUsuario.criar(usuarios);
                 usuarios.add(novo);
                 break;
               case 2:
                 /* Buscar Usuário por CPF */
-                  Usuario usuario = operacoes.buscarUsuario(usuarios);
-                  if(usuario != null) {
-                    JOptionPane.showMessageDialog(null, usuario);
-                  } else {
-                    JOptionPane.showMessageDialog(null,Operacoes.USUARIO_NAO_ENCONTRADO);
-                  }
+                  operacoesDeUsuario.consultarPorCPF(usuarios);
                 break;
               case 3:
-                operacoes.excluirUsuario(usuarios, reservas,emprestimos);
+                operacoesDeUsuario.excluir(usuarios, reservas,emprestimos);
                 break;
               case 4:
                 /* editar usuário */
                 boolean continuarEditarUsuario = true;
                 while(continuarEditarUsuario) {
-                  int opcaoEditarUsuario = operacoes.selecionarOpcaoDeEditarUsuario();
+                  int opcaoEditarUsuario = operacoesDeUsuario.selecionarOpcaoDeEditar();
                   switch (opcaoEditarUsuario) {
                     case 1:
                       /* Editar nome */
-                      operacoes.editarNomeDoUsuario(usuarios);
+                      operacoesDeUsuario.editarNome(usuarios);
                       break;
                     case 2:
                       /* Editar CPF */
-                      operacoes.editarCPFDoUsuario(usuarios);
+                      operacoesDeUsuario.editarCPF(usuarios);
                       break;
                     case 3:
                       /* Editar RGA/SIAPE */
-                      operacoes.editarRGAOuSIAPEDoUsuario(usuarios);
+                      operacoesDeUsuario.editarRGAOuSIAPE(usuarios);
                       break;
                     case 4:
                       /* Voltar */
@@ -69,14 +89,14 @@ public class Biblioteca {
                       break;
                     default:
                       /* opção invalida */
-                      JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
+                      JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
                       break;
 
                   }
                 }
                 break;
               case 5:
-                operacoes.listarUsuarios(usuarios);
+                operacoesDeUsuario.listar(usuarios);
                 break;
               case 6:
                 continuarUsuarios = false;
@@ -88,56 +108,52 @@ public class Biblioteca {
           }
           break;
         case 2:
+          OperacoesDeExemplar operacoesDeExemplares = new OperacoesGraficasDeExemplar();
           boolean continuarExemplares = true;
           while(continuarExemplares) {
-            int opcaoDeExemplares = operacoes.selecionarOpcaoDeExemplares();
+            int opcaoDeExemplares = operacoesDeExemplares.selecionarOpcao();
             switch (opcaoDeExemplares) {
               case 1:
                 /* cadastrar exemplar */
-                Exemplar novo = operacoes.criarExemplar();
+                Exemplar novo = operacoesDeExemplares.criar();
                 acervo.add(novo);
                 JOptionPane.showMessageDialog(null, "Exemplar criado com código #" +novo.getId() + ".");
                 break;
               case 2:
                 /* consultar por codigo */
-                Exemplar temp = operacoes.buscarExemplarPorCodigo(acervo);
-                if(temp == null) {
-                  JOptionPane.showMessageDialog(null, Operacoes.EXEMPLAR_NAO_ENCONTRADO);
-                  return;
-                }
-                JOptionPane.showMessageDialog(null, temp);
+                operacoesDeExemplares.consultarPorCodigo(acervo);
                 break;
               case 3:
-                operacoes.excluirExemplar(acervo,emprestimos, reservas);
+                operacoesDeExemplares.excluir(acervo,emprestimos, reservas);
                 break;
               case 4:
                 /* editar exemplar */
                 boolean continuarEditarExemplares = true;
                 while(continuarEditarExemplares) {
-                  int opcaoEditarExemplar = operacoes.selecionarOpcaoDeEditarExemplar();
+                  int opcaoEditarExemplar = operacoesDeExemplares.selecionarOpcaoDeEditar();
                   switch (opcaoEditarExemplar) {
                     case 1:
                       /* editar título */
-                      operacoes.editarTituloDoExemplar(acervo);
+                      operacoesDeExemplares.editarTitulo(acervo);
                       break;
                     case 2:
                       /* editar ano ou tipo de arquivo */
-                      operacoes.editarAnoOuTipoDeArquivoDoExemplar(acervo);
+                      operacoesDeExemplares.editarAnoOuTipoDeArquivo(acervo);
                       break;
                     case 3:
                       /* adicionar categoria */
-                      operacoes.adicionarCategoriaAoExemplar(acervo, categorias);
+                      operacoesDeExemplares.adicionarCategoria(acervo, categorias);
                       break;
                     case  4:
                       /* remover categoria */
-                      operacoes.removerCategoriaDoExemplar(acervo,categorias);
+                      operacoesDeExemplares.removerCategoria(acervo,categorias);
                       break;
                     case 5:
                       /* voltar */
                       continuarEditarExemplares = false;
                       break;
                     default:
-                      JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
+                      JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
                       break;
                   }
                 }
@@ -145,13 +161,13 @@ public class Biblioteca {
                 break;
               case 5:
                 /* listar todos os exemplares */
-                operacoes.listarExemplares(acervo);
+                operacoesDeExemplares.listar(acervo);
                 break;
               case 6:
                 continuarExemplares = false;
                 break;
               default:
-                JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
+                JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
                 break;
             }
           }
@@ -173,7 +189,7 @@ public class Biblioteca {
               case 2:
                 /* pesquisar por codigo */
                 Categoria temp = operacoes.buscarCategoriaPorCodigo(categorias);
-                if(temp == null) JOptionPane.showMessageDialog(null, Operacoes.CATEGORIA_NAO_ENCONTRADA);
+                if(temp == null) JOptionPane.showMessageDialog(null, OperacoesAntiga.CATEGORIA_NAO_ENCONTRADA);
                 else JOptionPane.showMessageDialog(null, temp);
                 break;
               case 3:
@@ -192,7 +208,7 @@ public class Biblioteca {
                 continuarCategorias = false;
                 break;
               default:
-                JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
+                JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
                 break;
             }
           }
@@ -227,7 +243,7 @@ public class Biblioteca {
                 continuarEmprestimos = false;
                 break;
               default:
-                JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
+                JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
                 break;
             }
           }
@@ -262,19 +278,19 @@ public class Biblioteca {
                 continuarReservas = false;
                 break;
               default:
-                JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
+                JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
                 break;
             }
           }
           break;
         case 6:
-          /* TODO Consultar total de exemplares reservados por categoria em um período */
+          operacoes.consultarTotalDeExemplaresReservadosPorCategoriaEmUmPeríodo(reservas,categorias);
           break;
         case 7:
           continuar = false;
           break;
         default:
-          JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
+          JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
           break;
       }
     }
