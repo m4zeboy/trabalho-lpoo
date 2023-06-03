03377dcb9dc046d9e2d2f9b42c1816bf8e1ba49f
m4zeboy <moises775azevedo@gmail.com>
2023-06-03 10:45:00 -0300

    Terminando a implementação da classe concreta para as operações de emprestimo em interface gráfica

--- src/biblioteca/operacoes/emprestimos/OperacoesGraficaDeEmprestimo.java
+++ src/biblioteca/operacoes/emprestimos/OperacoesGraficaDeEmprestimo.java
@@ -2,11 +2,12 @@ package biblioteca.operacoes.emprestimos;
 
 import biblioteca.Reserva;
 import biblioteca.emprestimo.Emprestimo;
+import biblioteca.emprestimo.Multa;
 import biblioteca.exemplar.Exemplar;
-import biblioteca.operacoes.OperacoesAntiga;
+import biblioteca.operacoes.PainelGrafico;
+import biblioteca.operacoes.excecoes.EmprestimoJaDevolvidoException;
 import biblioteca.operacoes.exemplares.OperacoesDeExemplar;
 import biblioteca.operacoes.usuario.OperacoesDeUsuario;
-import biblioteca.operacoes.usuario.OperacoesGraficasDeUsuario;
 import biblioteca.usuario.Usuario;
 
 import javax.swing.*;
@@ -16,7 +17,6 @@ public class OperacoesGraficaDeEmprestimo extends OperacoesDeEmprestimo {
   public int selecionarOpcao() {
     return Integer.parseInt(JOptionPane.showInputDialog(getMenu()));
   }
-
   public Emprestimo emprestar(ArrayList<Exemplar> acervo, ArrayList<Usuario> usuarios, ArrayList<Emprestimo> emprestimos, ArrayList<Reserva> reservas) {
     String cpf = JOptionPane.showInputDialog("CPF: ");
     Usuario usuario = OperacoesDeUsuario.buscarPorCPF(usuarios,cpf);
@@ -51,5 +51,77 @@ public class OperacoesGraficaDeEmprestimo extends OperacoesDeEmprestimo {
     }
     Emprestimo emprestimo = new Emprestimo(usuario,exemplar);
     return emprestimo;
+  }
+  public void consultarPorCodigo(ArrayList<Emprestimo> emprestimos) {
+    String codigo = JOptionPane.showInputDialog("Código do empréstimo: ");
+    try {
+      int id = Integer.parseInt(codigo);
+      Emprestimo emprestimo = buscarPorCodigo(emprestimos,id);
+      if(emprestimo == null) {
+        JOptionPane.showMessageDialog(null,OperacoesDeEmprestimo.EMPRESTIMO_NAO_ENCONTRADO);
+        return;
+      }
+      JOptionPane.showMessageDialog(null, emprestimo);
+    } catch (NumberFormatException exception) {
+      JOptionPane.showMessageDialog(null, "O valor informado precisa ser um número inteiro.");
+    }
+  }
+  public void devolver(ArrayList<Emprestimo> emprestimos, ArrayList<Multa> multas) {
+    String codigo = JOptionPane.showInputDialog("Código do empréstimo: ");
+    try {
+      int id = Integer.parseInt(codigo);
+      Emprestimo emprestimo = buscarPorCodigo(emprestimos, id);
+      if(emprestimo == null) {
+        JOptionPane.showMessageDialog(null,OperacoesDeEmprestimo.EMPRESTIMO_NAO_ENCONTRADO);
+        return;
+      }
+      try {
+        Multa multa = emprestimo.devolver();
+        if(multa != null) {
+          multas.add(multa);
+          JOptionPane.showMessageDialog(null, "Empréstimo devolvido com atraso. Multa de R$" + multa.getValor() + ".");
+          return;
+        }
+        JOptionPane.showMessageDialog(null, "Empréstimo devolvido.");
+      } catch (EmprestimoJaDevolvidoException exception) {
+        JOptionPane.showMessageDialog(null,exception.getMessage());
+      }
+    } catch (NumberFormatException exception) {
+      JOptionPane.showMessageDialog(null, "O valor informado precisa ser um número inteiro.");
+    }
+  }
+  public void renovar(ArrayList<Emprestimo> emprestimos, ArrayList<Reserva> reservas) {
+    String codigo = JOptionPane.showInputDialog("Código do empréstimo: ");
+    try {
+      int id = Integer.parseInt(codigo);
+      Emprestimo emprestimo = buscarPorCodigo(emprestimos, id);
+      if(emprestimo == null) {
+        JOptionPane.showMessageDialog(null, OperacoesDeEmprestimo.EMPRESTIMO_NAO_ENCONTRADO);
+        return;
+      }
+      if(emprestimo.getExemplar().temReservasAtivas(reservas)) {
+        JOptionPane.showMessageDialog(null, "Não é possível renovar o empréstimo pois o exemplar já está reservado.");
+        return;
+      }
+      boolean renovou = emprestimo.renovar();
+      if(!renovou) {
+        JOptionPane.showMessageDialog(null, "Não é possível renovar o emprestimo.");
+        return;
+      }
+      JOptionPane.showMessageDialog(null, "Emprestimo #" + emprestimo.getId() + " renovado.");
+    } catch (NumberFormatException exception) {
+      JOptionPane.showMessageDialog(null, "O valor informado precisa ser um número inteiro.");
+    }
+  }
+  public void listar(ArrayList<Emprestimo> emprestimos) {
+    if(emprestimos.size() == 0) {
+      JOptionPane.showMessageDialog(null, "Não há empréstimos.");
+    }
+    String saida = "";
+    for(Emprestimo emprestimo: emprestimos) {
+      saida += emprestimo;
+      saida +=   "====================================================\n";
+    }
+    PainelGrafico.mostrarMensagemComScroll("Lista de empréstimos",saida);
   }
 }
