38eb7118d1f17b46a00f1e172436565b4e9aacd2
m4zeboy <moises775azevedo@gmail.com>
2023-06-03 11:55:48 -0300

    Atualizando as operações de reserva para a nova interface.

--- src/biblioteca/Biblioteca.java
+++ src/biblioteca/Biblioteca.java
@@ -1,6 +1,7 @@
 package biblioteca;
 
 import biblioteca.emprestimo.Emprestimo;
+import biblioteca.emprestimo.Multa;
 import biblioteca.exemplar.Exemplar;
 import biblioteca.operacoes.categorias.OperacoesDeCategoria;
 import biblioteca.operacoes.categorias.OperacoesGraficasDeCategoria;
@@ -8,13 +9,16 @@ import biblioteca.operacoes.emprestimos.OperacoesDeEmprestimo;
 import biblioteca.operacoes.emprestimos.OperacoesGraficaDeEmprestimo;
 import biblioteca.operacoes.exemplares.OperacoesDeExemplar;
 import biblioteca.operacoes.exemplares.OperacoesGraficasDeExemplar;
+import biblioteca.operacoes.reservas.OperacoesDeReserva;
+import biblioteca.operacoes.reservas.OperacoesGraficasDeReserva;
 import biblioteca.operacoes.usuario.OperacoesDeUsuario;
 import biblioteca.operacoes.usuario.OperacoesGraficasDeUsuario;
 import biblioteca.usuario.Usuario;
 
 import javax.swing.JOptionPane;
 import java.util.ArrayList;
-import biblioteca.operacoes.*;
+import biblioteca.operacoes.Operacoes;
+import biblioteca.operacoes.Grafica;
 public class Biblioteca {
   public static void main(String[] args) {
     ArrayList<Usuario> usuarios = new ArrayList<>();
@@ -22,16 +26,23 @@ public class Biblioteca {
     ArrayList<Categoria> categorias = new ArrayList<>();
     ArrayList<Emprestimo> emprestimos = new ArrayList<>();
     ArrayList<Reserva> reservas = new ArrayList<>();
+    ArrayList<Multa> multas = new ArrayList<>();
 
     Operacoes menuPrincipal = new Grafica();
+    OperacoesDeUsuario operacoesDeUsuario = new OperacoesGraficasDeUsuario();
+    OperacoesDeExemplar operacoesDeExemplar = new OperacoesGraficasDeExemplar();
+    OperacoesDeCategoria operacoesDeCategoria = new OperacoesGraficasDeCategoria();
+    OperacoesDeEmprestimo operacoesDeEmprestimo = new OperacoesGraficaDeEmprestimo();
+    OperacoesDeReserva operacoesDeReserva = new OperacoesGraficasDeReserva();
 
     Semente.semear(usuarios, acervo, categorias, emprestimos, reservas);
-    // todo A classe Grafica deveria ser separada em outras mais específicas, cada qual gerenciando a sua parte.
-    //    todo classe de operações de exemplares
-    //    todo classe de operações de categorias
-    //    todo classe de operações de emprestimos
-    //    todo classe de operações de reservas
 
+    // todo alterar as funções getMenu para capturar uma exceção caso a conversão para inteiro falhe.
+    //    todo getMenu usuario
+    //      todo getMenuEditar
+    //    todo getMenu exemplar
+    //    todo getMenu Categoria
+    //    todo getMenu Emprestimo
 
     // todo Na classe Biblioteca eu recomendaria o uso do switch para os casos do menu, aumenta a legibilidade do código, especialmente ao utilizar o formato mais atual do JAVA.
 
@@ -52,7 +63,6 @@ public class Biblioteca {
       int opcaoPrincipal = menuPrincipal.selecionarOpcao();
       switch (opcaoPrincipal) {
         case 1:
-          OperacoesDeUsuario operacoesDeUsuario = new OperacoesGraficasDeUsuario();
           boolean continuarUsuarios = true;
           while(continuarUsuarios) {
             int opcaoDeUsuarios = operacoesDeUsuario.selecionarOpcao();
@@ -93,7 +103,7 @@ public class Biblioteca {
                       break;
                     default:
                       /* opção invalida */
-                      JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
+                      JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
                       break;
 
                   }
@@ -112,52 +122,51 @@ public class Biblioteca {
           }
           break;
         case 2:
-          OperacoesDeExemplar operacoesDeExemplares = new OperacoesGraficasDeExemplar();
           boolean continuarExemplares = true;
           while(continuarExemplares) {
-            int opcaoDeExemplares = operacoesDeExemplares.selecionarOpcao();
+            int opcaoDeExemplares = operacoesDeExemplar.selecionarOpcao();
             switch (opcaoDeExemplares) {
               case 1:
                 /* cadastrar exemplar */
-                Exemplar novo = operacoesDeExemplares.criar();
+                Exemplar novo = operacoesDeExemplar.criar();
                 acervo.add(novo);
                 JOptionPane.showMessageDialog(null, "Exemplar criado com código #" +novo.getId() + ".");
                 break;
               case 2:
                 /* consultar por codigo */
-                operacoesDeExemplares.consultarPorCodigo(acervo);
+                operacoesDeExemplar.consultarPorCodigo(acervo);
                 break;
               case 3:
-                operacoesDeExemplares.excluir(acervo,emprestimos, reservas);
+                operacoesDeExemplar.excluir(acervo,emprestimos, reservas);
                 break;
               case 4:
                 /* editar exemplar */
                 boolean continuarEditarExemplares = true;
                 while(continuarEditarExemplares) {
-                  int opcaoEditarExemplar = operacoesDeExemplares.selecionarOpcaoDeEditar();
+                  int opcaoEditarExemplar = operacoesDeExemplar.selecionarOpcaoDeEditar();
                   switch (opcaoEditarExemplar) {
                     case 1:
                       /* editar título */
-                      operacoesDeExemplares.editarTitulo(acervo);
+                      operacoesDeExemplar.editarTitulo(acervo);
                       break;
                     case 2:
                       /* editar ano ou tipo de arquivo */
-                      operacoesDeExemplares.editarAnoOuTipoDeArquivo(acervo);
+                      operacoesDeExemplar.editarAnoOuTipoDeArquivo(acervo);
                       break;
                     case 3:
                       /* adicionar categoria */
-                      operacoesDeExemplares.adicionarCategoria(acervo, categorias);
+                      operacoesDeExemplar.adicionarCategoria(acervo, categorias);
                       break;
                     case  4:
                       /* remover categoria */
-                      operacoesDeExemplares.removerCategoria(acervo,categorias);
+                      operacoesDeExemplar.removerCategoria(acervo,categorias);
                       break;
                     case 5:
                       /* voltar */
                       continuarEditarExemplares = false;
                       break;
                     default:
-                      JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
+                      JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
                       break;
                   }
                 }
@@ -165,20 +174,19 @@ public class Biblioteca {
                 break;
               case 5:
                 /* listar todos os exemplares */
-                operacoesDeExemplares.listar(acervo);
+                operacoesDeExemplar.listar(acervo);
                 break;
               case 6:
                 continuarExemplares = false;
                 break;
               default:
-                JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
+                JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
                 break;
             }
           }
           break;
         case 3:
           /* categorias */
-          OperacoesDeCategoria operacoesDeCategoria = new OperacoesGraficasDeCategoria();
           boolean continuarCategorias = true;
           while(continuarCategorias) {
             int opcaoDeCategorias = operacoesDeCategoria.selecionarOpcao();
@@ -211,13 +219,12 @@ public class Biblioteca {
                 continuarCategorias = false;
                 break;
               default:
-                JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
+                JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
                 break;
             }
           }
           break;
         case 4:
-          OperacoesDeEmprestimo operacoesDeEmprestimo = new OperacoesGraficaDeEmprestimo();
           boolean continuarEmprestimos = true;
           while(continuarEmprestimos) {
             int opcaoDeEmprestimos = operacoesDeEmprestimo.selecionarOpcao();
@@ -231,23 +238,23 @@ public class Biblioteca {
                 }
                 break;
               case 2:
-                operacoes.consultaStatusDeUmEmprestimo(emprestimos);
+                operacoesDeEmprestimo.consultarPorCodigo(emprestimos);
                 break;
               case 3:
-                operacoes.devolverEmprestimo(emprestimos);
+                operacoesDeEmprestimo.devolver(emprestimos,multas);
                 break;
               case 4:
-                operacoes.renovarEmprestimo(emprestimos, reservas);
+                operacoesDeEmprestimo.renovar(emprestimos, reservas);
                 break;
               case 5:
-                operacoes.listarEmprestimos(emprestimos);
+                operacoesDeEmprestimo.listar(emprestimos);
                 break;
               case 6:
                 /* voltar */
                 continuarEmprestimos = false;
                 break;
               default:
-                JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
+                JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
                 break;
             }
           }
@@ -255,46 +262,37 @@ public class Biblioteca {
         case 5:
           boolean continuarReservas = true;
           while(continuarReservas) {
-            int opcaoDeReservas = operacoes.selecionarOpcaoDeReservas();
+            int opcaoDeReservas = operacoesDeReserva.selecionarOpcao();
             switch (opcaoDeReservas) {
               case 1:
-                Reserva reserva = operacoes.reservar(reservas, usuarios, acervo,emprestimos);
-                if(reserva != null) {
-                  reservas.add(reserva);
-                  JOptionPane.showMessageDialog(null, "Reserva criada com código #" + reserva.getId() + ".");
-                }
+                operacoesDeReserva.reservar(reservas, usuarios, acervo,emprestimos);
                 break;
               case 2:
-                Reserva temp = operacoes.buscarReservaPorCodigo(reservas);
-                if(temp != null) {
-                  JOptionPane.showMessageDialog(null, temp);
-                } else {
-                  JOptionPane.showMessageDialog(null, "Reserva não encontrada.");
-                }
+                operacoesDeReserva.consultarPorCodigo(reservas);
                 break;
               case 3:
-                operacoes.cancelarReserva(reservas);
+                operacoesDeReserva.cancelar(reservas);
                 break;
               case 4:
-                operacoes.listarReservasAtivasParaUmExemplar(acervo, reservas);
+                operacoesDeReserva.listarReservasAtivasParaUmExemplar(acervo, reservas);
                 break;
               case 5:
                 continuarReservas = false;
                 break;
               default:
-                JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
+                JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
                 break;
             }
           }
           break;
         case 6:
-          operacoes.consultarTotalDeExemplaresReservadosPorCategoriaEmUmPeríodo(reservas,categorias);
+          operacoesDeReserva.consultarTotalDeExemplaresReservadosPorCategoriaEmUmPeríodo(reservas,categorias);
           break;
         case 7:
           continuar = false;
           break;
         default:
-          JOptionPane.showMessageDialog(null, OperacoesAntiga.OPCAO_INVALIDA);
+          JOptionPane.showMessageDialog(null, Operacoes.OPCAO_INVALIDA);
           break;
       }
     }
