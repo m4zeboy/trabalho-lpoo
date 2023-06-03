eb32e406321d13ee494539422dc294a0dfbcea7c
m4zeboy <moises775azevedo@gmail.com>
2023-06-03 10:44:38 -0300

    Terminando a classe abstrata para as operações de emprestimo

--- src/biblioteca/operacoes/emprestimos/OperacoesDeEmprestimo.java
+++ src/biblioteca/operacoes/emprestimos/OperacoesDeEmprestimo.java
@@ -2,12 +2,14 @@ package biblioteca.operacoes.emprestimos;
 
 import biblioteca.Reserva;
 import biblioteca.emprestimo.Emprestimo;
+import biblioteca.emprestimo.Multa;
 import biblioteca.exemplar.Exemplar;
 import biblioteca.usuario.Usuario;
 
 import java.util.ArrayList;
 
 public abstract class OperacoesDeEmprestimo {
+  public static String EMPRESTIMO_NAO_ENCONTRADO = "Empréstimo não encontrado.";
   protected String getMenu() {
     String mensagem = "1 - Emprestar\n";
     mensagem += "2 - Consultar status de um empréstimo\n";
@@ -19,4 +21,14 @@ public abstract class OperacoesDeEmprestimo {
   }
   public abstract int selecionarOpcao();
   public abstract Emprestimo emprestar(ArrayList<Exemplar> acervo, ArrayList<Usuario> usuarios, ArrayList<Emprestimo> emprestimos, ArrayList<Reserva> reservas);
+  public static Emprestimo buscarPorCodigo(ArrayList<Emprestimo> emprestimos, int codigo) {
+    for(Emprestimo emprestimo: emprestimos) {
+      if(emprestimo.getId() == codigo) return emprestimo;
+    }
+    return null;
+  }
+  public abstract void consultarPorCodigo(ArrayList<Emprestimo> emprestimos);
+  public abstract void devolver(ArrayList<Emprestimo> emprestimos, ArrayList<Multa> multas);
+  public abstract void renovar(ArrayList<Emprestimo> emprestimos, ArrayList<Reserva> reserva);
+  public abstract void listar(ArrayList<Emprestimo> emprestimos);
 }
