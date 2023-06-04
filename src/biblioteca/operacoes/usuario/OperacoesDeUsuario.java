eeded102f1f79cc4e1e4f55498fd59e397d00912
m4zeboy <moises775azevedo@gmail.com>
2023-06-04 15:30:52 -0300

    Mudando os menus de usuário para string builder

--- src/biblioteca/operacoes/usuario/OperacoesDeUsuario.java
+++ src/biblioteca/operacoes/usuario/OperacoesDeUsuario.java
@@ -10,34 +10,22 @@ import java.util.ArrayList;
 
 public abstract class OperacoesDeUsuario {
   public static String USUARIO_NAO_ENCONTRADO= "Usuário não encontrado";
-  protected String getMenu() {
-    String mensagem = "1 - Cadastrar\n";
-    mensagem += "2 - Pesquisar por CPF\n";
-    mensagem += "3 - Excluir\n";
-    mensagem += "4 - Editar\n";
-    mensagem +=  "5 - Listar todos os cadastros\n";
-    mensagem +=  "6 - Voltar\n\n";
-    return mensagem;
-  }
-  public abstract int selecionarOpcao();
-  public abstract Usuario criar(ArrayList<Usuario> usuarios);
-  public abstract void listar(ArrayList<Usuario> usuarios);
-  public abstract void consultarPorCPF(ArrayList<Usuario> usuarios);
-  public abstract Usuario buscarPorCPF(ArrayList<Usuario> usuarios);
-  public static Servidor buscarPorSiape(ArrayList<Usuario> usuarios, String siape) {
+
+  /* Métodos estáticos úteis para outras classes de operações, que não dependem de interface (gráfica ou terminal) */
+  public static Servidor buscarPorSiape(ArrayList<Usuario> usuarios, int siape) {
     for(Usuario usuario: usuarios) {
       if(usuario instanceof Servidor) {
-        if(((Servidor) usuario).getSiape() == Integer.parseInt(siape)) {
+        if(((Servidor) usuario).getSiape() == siape) {
           return ((Servidor) usuario);
         }
       }
     }
     return null;
   }
-  public static Aluno buscarPorRga(ArrayList<Usuario> usuarios, String rga) {
+  public static Aluno buscarPorRga(ArrayList<Usuario> usuarios, int rga) {
     for(Usuario usuario: usuarios) {
       if(usuario instanceof Aluno) {
-        if(((Aluno) usuario).getRga() == Integer.parseInt(rga)) {
+        if(((Aluno) usuario).getRga() == rga) {
           return ((Aluno) usuario);
         }
       }
@@ -50,12 +38,29 @@ public abstract class OperacoesDeUsuario {
     }
     return null;
   }
-  public static String getMenuEditar() {
-    String mensagem = "1 - Editar Nome\n";
-    mensagem += "2 - Editar CPF\n";
-    mensagem += "3 - Editar RGA/SIAPE\n";
-    mensagem += "4 - Voltar\n\n";
-    return mensagem;
+  /* a função getMenu() obtem o menu de gerência de usuários no formato de string */
+  protected String getMenu() {
+    StringBuilder menu = new StringBuilder();
+    menu.append("1 - Cadastrar\n");
+    menu.append("2 - Pesquisar por CPF\n");
+    menu.append("3 - Excluir\n");
+    menu.append("4 - Editar\n");
+    menu.append("5 - Listar todos os cadastros\n");
+    menu.append("6 - Voltar\n\n");
+    return menu.toString();
+  }
+  public abstract int selecionarOpcao();
+
+  public abstract void criar(ArrayList<Usuario> usuarios);
+  public abstract void listar(ArrayList<Usuario> usuarios);
+  public abstract void consultarPorCPF(ArrayList<Usuario> usuarios);
+  public abstract Usuario buscarPorCPF(ArrayList<Usuario> usuarios);
+  public String getMenuEditar() {
+    StringBuilder mensagem = new StringBuilder("1 - Editar Nome\n");
+    mensagem.append("2 - Editar CPF\n");
+    mensagem.append("3 - Editar RGA/SIAPE\n");
+    mensagem.append("4 - Voltar\n\n");
+    return mensagem.toString();
   }
   public abstract int selecionarOpcaoDeEditar();
   public abstract void editarNome(ArrayList<Usuario> usuarios);
